493073f9679bba917c9855d278686410a1df25ff
Johnson Wang <yuhang0229@gmail.com>
2026-09-25 20:04:53 +1000

    fix: BaseEntity

--- backend/Grove.Domain/Common/BaseEntity.cs
+++ backend/Grove.Domain/Common/BaseEntity.cs
@@ -5,4 +5,7 @@ public abstract class BaseEntity
   public long Id { get; protected set; }
   public DateTime CreatedAt { get; protected set; }
   public DateTime UpdatedAt { get; protected set; }
+
+  public void SetCreatedAt(DateTime value) => CreatedAt = value;
+  public void SetUpdatedAt(DateTime value) => UpdatedAt = value;
 }
